d1da3dafd709d93914c56ade856d7a0724716e4b
Leonid Kozhinov <lkozhinov@toloka.ai>
2025-05-14 19:00:43 +0000

    Update dagger to 0.18.7

--- test_agent/src/test_agent/main.py
+++ test_agent/src/test_agent/main.py
@@ -1,4 +1,5 @@
 import logging
+from textwrap import dedent
 
 from dagger import dag, function, object_type
 from rich.logging import RichHandler
@@ -16,18 +17,28 @@ class TestAgent:
 FROM ubuntu:jammy
 RUN echo1 "Hello, world!"
 """
-        ws = dag.workspace()
+        env = (
+            dag.env()
+            .with_workspace_input(
+                'before',
+                dag.workspace().write_file('Dockerfile', dockerfile),
+                'workspace with tools to complete assignment',
+            )
+            .with_workspace_output('after', 'workspace with completed assignment')
+        )
         llm = (
             dag.llm(max_api_calls=100)
-            .with_workspace(ws.write_file('Dockerfile', dockerfile))
-            .with_prompt("""
-You're a senior developer. You have an access to a workspace.
-There is a Dockerfile in the workspace by the path Dockerfile.
-Your task is to fix the Dockerfile and write it to Dockerfile.
-For that task you can build the Dockerfile and check the result.
-Don't stop until the Dockerfile is valid.
-""")
+            .with_env(env)
+            .with_prompt(
+                dedent("""
+            You're a senior developer. You have an access to a workspace.
+            There is a Dockerfile in the workspace by the path Dockerfile.
+            Your task is to fix the Dockerfile and write it to workspace.
+            For that task you can build the Dockerfile and check the result.
+            Return the workspace with the fixed Dockerfile.
+            Don't stop until the Dockerfile is valid.
+            """)
+            )
         )
-        llm = await llm.sync()
-        result = llm.workspace().container()
+        result = llm.env().output('after').as_workspace().container()
         return await result.file('Dockerfile').contents()
